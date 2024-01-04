eb4f450630e83f16fd7d9c7900bbe5ee20b1ad74
josuawilliams <josuawilliams17@gmail.com>
2024-01-04 09:36:04 +0700

    flutter

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Development" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\Joe\\latihan_flutter\\learn_flutter" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Development"
+  "PROJECT_DIR=D:\\Joe\\latihan_flutter\\learn_flutter"
+  "FLUTTER_ROOT=C:\\Development"
+  "FLUTTER_EPHEMERAL_DIR=D:\\Joe\\latihan_flutter\\learn_flutter\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\Joe\\latihan_flutter\\learn_flutter"
+  "FLUTTER_TARGET=D:\\Joe\\latihan_flutter\\learn_flutter\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9iNGZiMTEyMTRkZDJkZGE2Y2UwMTJkZDk4ZWE0OThlOWU4YjkxMjYyLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\Joe\\latihan_flutter\\learn_flutter\\.dart_tool\\package_config.json"
+)
